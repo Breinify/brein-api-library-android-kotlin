75cac043cd70e6474bda9b45d16e937c951c48f9
Marco Recchioni <m.recchioni@me.com>
2020-12-04 21:26:17 +0100

    code cleanUp

--- api/src/main/java/com/brein/api/BreinInvalidConfigurationException.kt
+++ api/src/main/java/com/brein/api/BreinInvalidConfigurationException.kt
@@ -4,9 +4,6 @@ package com.brein.api
  * Exception in case of wrong configuration
  */
 class BreinInvalidConfigurationException : RuntimeException {
-    /**
-     * Exception methods...
-     */
     constructor(e: Throwable?) : super(e) {}
     constructor(msg: String?) : super(msg) {}
     constructor(msg: String?, cause: Exception?) : super(msg, cause) {}
