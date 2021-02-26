3d93f186aa078089565c2f161843279f9bc7bea0
Marco Recchioni <m.recchioni@me.com>
2021-02-26 13:48:52 +0100

    code cleanUp

--- api/src/main/java/com/brein/api/BreinPushNotificationService.kt
+++ api/src/main/java/com/brein/api/BreinPushNotificationService.kt
@@ -2,13 +2,11 @@ package com.brein.api
 
 import android.annotation.SuppressLint
 import android.app.Notification
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
 import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
