87ab4f2b5a2f1908830c673cde8f1e00578ffe58
Marco Recchioni <m.recchioni@me.com>
2021-04-28 12:54:32 +0200

    new constants added

---
+++ api/src/main/java/com/brein/domain/BreinifyNotificationConstant.kt
@@ -0,0 +1,10 @@
+package com.brein.domain
+
+object BreinifyNotificationConstant {
+
+    const val BREIN_PAYLOAD = "breinPayload"
+    const val BREIN_SEGEMENT = "breinify"
+    const val BREIN_CAMPAIGN_SEGMENT = "campaign"
+    const val BREIN_NOTIFICATION_ID = "notificationId"
+
+}
