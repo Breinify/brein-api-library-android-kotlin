12ee473da8112d19d2a1c311d87324c397a3f071
Marco Recchioni <m.recchioni@me.com>
2021-04-29 11:20:34 +0200

    constants added

--- api/src/main/java/com/brein/domain/BreinNotificationAction.kt
+++ api/src/main/java/com/brein/domain/BreinNotificationAction.kt
@@ -4,4 +4,6 @@ object BreinNotificationAction {
     const val OPENED_FIRST = "notification_first_open"
     const val OPENED_SECOND = "notification_second_open"
     const val IGNORE = "notification_cancelled"
+    const val DEFAULT = "default"
+    const val NO_ACTION = "noAction"
 }
